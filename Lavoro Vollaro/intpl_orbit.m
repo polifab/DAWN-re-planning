ca5b530f263dee4d44bb9845f175a5604b062442
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2020-09-09 16:40:23 +0200

    Legibility improvements
    Deleted unused parts.
    Improved 'help' section.

--- Lavoro Vollaro/intpl_orbit.m
+++ Lavoro Vollaro/intpl_orbit.m
@@ -4,13 +4,16 @@ function orb = intpl_orbit(arr_days,r_init,v_init)
 %   around the Sun in ARR_DAYS days, starting at position R_INIT
 %   with velocity V_INIT.
 %   It uses the patched conics method.
+%
 %   orb = INTPL_ORBIT(...) returns the points composing the orbit.
 %
 %   It uses rkf45 to numerically integrate Equation 2.22 in
 %   "Orbital Mechanics for Engineering Students" - Howard D. Curtis.
 %
 %   arr_days  - days needed for the spacecraft to arrive at destination
+%
 %   r_init    - initial position of the spacecraft
+%
 %   v_init    - initial velocity of the spacecraft
 %
 % User M-function required:   rkf45
@@ -18,7 +21,7 @@ function orb = intpl_orbit(arr_days,r_init,v_init)
 
     %% Constants
     global mu
-    hours = 3600; % [s]
+    hours = 3600; %[s]
     Sun_radius = 695508; %[km]   
 
     %% Input data:
@@ -87,45 +90,17 @@ function orb = intpl_orbit(arr_days,r_init,v_init)
       This function computes the maximum and minimum radii, the times they
       occur and and the speed at those times. It prints those results to
       the command window and plots the orbit.
-
-      r         - magnitude of the position vector at the times in t
-      imax      - the component of r with the largest value
-      rmax      - the largest value of r
-      imin      - the component of r with the smallest value
-      rmin      - the smallest value of r
-      v_at_rmax - the speed where r = rmax
-      v_at_rmin - the speed where r = rmin
-
-      User subfunction required: light_gray
     %}
         
-        for i = 1:length(t)
-            r(i) = norm([y(i,1) y(i,2) y(i,3)]);
-        end
-
-        [rmax, imax] = max(r);
-        [rmin, imin] = min(r);
-
-        v_at_rmax   = norm([y(imax,4) y(imax,5) y(imax,6)]);
-        v_at_rmin   = norm([y(imin,4) y(imin,5) y(imin,6)]);
-
         %% Output to the command window:
         fprintf('\n\n--------------------------------------------------------\n')
         fprintf('\n Spacecraft Orbit\n')
-        fprintf(' %s\n', datestr([2007,9,27,0,0,0]))
         fprintf('\n The initial position is [%g, %g, %g] (km).',...
                                                              r0(1), r0(2), r0(3))
         fprintf('\n   Magnitude = %g km\n', norm(r0))
         fprintf('\n The initial velocity is [%g, %g, %g] (km/s).',...
                                                              v0(1), v0(2), v0(3))
         fprintf('\n   Magnitude = %g km/s\n', norm(v0))
-        fprintf('\n Initial time = %g h.\n Final time   = %g h.\n',0,tf/hours) 
-        fprintf('\n The minimum altitude is %g km at time = %g h.',...
-                    rmin-R, t(imin)/hours)
-        fprintf('\n The speed at that point is %g km/s.\n', v_at_rmin)
-        fprintf('\n The maximum altitude is %g km at time = %g h.',...
-                    rmax-R, t(imax)/hours)
-        fprintf('\n The speed at that point is %g km/s\n', v_at_rmax)
         fprintf('\n--------------------------------------------------------\n\n')
 
         %% Plot the results:
