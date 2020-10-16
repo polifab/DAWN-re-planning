d99494333da64db40c64aa09d28f23b5c0270479
Simone Vollaro <39267642+slim71@users.noreply.github.com>
2020-10-16 10:50:57 +0200

    Fix for Vesta/Ceres data
    Gathered data from Horizons.
    Created python script to parse said data.
    Created MATLAB functions to load data.
    
    WARNING: data functions for Vesta/Ceres data are heavy to show in the MATLAB Editor.

---
+++ Lavoro Vollaro/rates_try/object_vectors/search.py
@@ -0,0 +1,44 @@
+#! usr/bin/python
+
+import os
+
+os.chdir("FILES DIRECTORY")
+
+ceres = open("ceres_rv.m", "x")
+vesta = open("vesta_rv.m", "x")
+
+with open('ceres_vector.txt', 'rt') as myfile, open("ceres_rv.m", "w") as ceres:
+    ceres.write("ceres_data = [ \n")
+    for myline in myfile:
+        line = myline.strip('\n')
+        if len(line) >= 2:
+            if line[1] == 'X':
+                line = line.replace("X =", "")
+                line = line.replace("Y =", "")
+                line = line.replace("Z =", "")
+                ceres.write(line)
+            elif line[1] == 'V':
+                line = line.replace("VX=", "")
+                line = line.replace("VY=", "")
+                line = line.replace("VZ=", "")
+                ceres.write(line)
+                ceres.write('\n')
+    ceres.write('];')
+
+with open('vesta_vector.txt', 'rt') as myfile, open("vesta_rv.m", "w") as vesta:
+    vesta.write("vesta_data = [ \n")
+    for myline in myfile:
+        line = myline.strip('\n')
+        if len(line) >= 2:
+            if line[1] == 'X':
+                line = line.replace("X =", "")
+                line = line.replace("Y =", "")
+                line = line.replace("Z =", "")
+                vesta.write(line)
+            elif line[1] == 'V':
+                line = line.replace("VX=", "")
+                line = line.replace("VY=", "")
+                line = line.replace("VZ=", "")
+                vesta.write(line)
+                vesta.write('\n')
+    vesta.write('];')
